f1c0b0174a7f647d14cc081e5d41686fd8163a5f
Sam Pal <sam.pal@10gen.com>
2019-07-17 10:38:33 -0400

    add express

--- mainpage.js
+++ mainpage.js
@@ -1,6 +1,26 @@
 'use strict';
+const express = require('express');
+const port = 3000;
+const path = require('path');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-function display_button() {
-    //   document.getElementById("category")
-    //  <button type="button" class="btn btn-success">Success</button>
-}
+const uri = 'mongodb+srv://sam:sam1@cluster0-nge8c.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'leafie_awards';
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  if (err) console.log(err);
+
+  const db = client.db(dbName);
+  const people = db.collection('people');
+  const superlatives = db.collection('superlatives');
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+});
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/mainpage.html');
+});
