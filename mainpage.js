5ec614e54523cee0453b709991e92dd383205e95
Sam Pal <sam.pal@10gen.com>
2019-07-18 15:57:51 -0400

    changed some html things, currently fighting with bodyParser

--- mainpage.js
+++ mainpage.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 const MongoClient = require('mongodb').MongoClient;
@@ -11,14 +10,18 @@ const uri = 'mongodb+srv://sam:sam1@cluster0-nge8c.mongodb.net/test?retryWrites=
 const dbName = 'leafie_awards';
 let peopleArray = [];
 let superlativesArray = [];
+let db;
+let people;
+let superlatives;
 
-app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+// app.use(bodyParser.text({ type: 'text/html' }))
 
 MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
   if (err) console.log(err);
 
-  const db = client.db(dbName);
+  db = client.db(dbName);
   const people = db.collection('people');
   const superlatives = db.collection('superlatives');
 
@@ -28,7 +31,6 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
     superlatives.find().forEach((doc) => {
       superlativesArray.push(doc);
     }).then(() => {
-        console.log("in here")
         client.close()
     });
   });
@@ -41,3 +43,25 @@ app.engine('html', require('ejs').renderFile);
 app.get('/leafie-awards', (req, res) => {
   res.render(__dirname + '/index.html', { people: peopleArray, superlatives: superlativesArray });
 });
+
+app.post('/form', (req, res) => {
+  // console.log('req.body.person', req.body.person);
+  // console.log('req.body.general', req.body.general);
+  // console.log('req.body.work', req.body.work);
+  // console.log('req.body.sports_games', req.body.sports_games);
+  // console.log('req.body.outside_work', req.body.outside_work);
+  res.send(req.body.sports_games);
+});
+
+// app.post('/nominated', (req, res) => {
+//   console.log('nominates');
+//   // people.updateOne( { name: "Sam Pal" }, { $set : { "nominations" : ["Best Whatever", "Great Person", "something something", "anotha 1"] }})
+//   res.sendStatus(201);
+// });
+//
+// app.get('/nominate', (req, res) => {
+//   people.find().toArray((err, result) => {
+//     if (err) console.log(err);
+//     res.rend(result)
+//   });
+// });
