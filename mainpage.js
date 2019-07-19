284dabeeeae0b6fa1c02085435d0e6348f0302ae
Sam Pal <sam.pal@10gen.com>
2019-07-19 01:44:15 -0400

    auth seems to work promises are a lil sketchy tho

--- mainpage.js
+++ mainpage.js
@@ -1,4 +1,6 @@
 'use strict';
+require('dotenv').config()
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -18,6 +20,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: 'application/json' }))
 app.use(bodyParser.json())
 
+//auth
+const session = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+
+// session support is required to use ExpressOIDC
+app.use(session({
+  secret: process.env.APP_SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
+
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+  scope: 'openid profile'
+});
+
+// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
+app.use(oidc.router);
+
+oidc.on('ready', () => {
+  app.listen(3000, () => console.log(`Started!`));
+});
+
+oidc.on('error', err => {
+  console.log('Unable to configure ExpressOIDC', err);
+});
+
 MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
   if (err) console.log(err);
 
@@ -35,16 +67,16 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
     });
   });
 
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  // app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 });
 
 app.engine('html', require('ejs').renderFile);
 
-app.get('/nominate', (req, res) => {
+app.get('/nominate', oidc.ensureAuthenticated(), (req, res) => {
   res.render(__dirname + '/index.html', { people: peopleArray, superlatives: superlativesArray });
 });
 
-app.post('/nominate', (req, res) => {
+app.post('/nominate', oidc.ensureAuthenticated(), (req, res) => {
   const name = req.body.person;
   const work = Array.isArray(req.body.work) ? req.body.work : [req.body.work];
   const sports_games = Array.isArray(req.body.sports_games) ? req.body.sports_games : [req.body.sports_games];
@@ -52,13 +84,17 @@ app.post('/nominate', (req, res) => {
   const general = Array.isArray(req.body.general) ? req.body.general : [req.body.general];
   const noms = [...work, ...sports_games, ...outside_work, ...general].filter(x => x);
 
+  const user = req.userContext.userinfo.name;
+
   people.findOne({ name : name }).then((result) => {
-    const dict = result.nominations || {};
-    for (let i = 0; i < noms.length; i++) {
-      if (dict[noms[i]]) dict[noms[i]] += 1;
-      else dict[noms[i]] = 1;
+    if (!result.voters || !result.voters.contains(user)) {
+      const dict = result.nominations || {};
+      for (let i = 0; i < noms.length; i++) {
+        if (dict[noms[i]]) dict[noms[i]] += 1;
+        else dict[noms[i]] = 1;
+      }
+      people.updateOne( { name: name }, { $set : { nominations : dict }, $push : { voters : user }})
     }
-    people.updateOne( { name: name }, { $set : { nominations : dict }})
   });
   // res.status(204).send();
   res.render(__dirname + '/index.html', { people: peopleArray, superlatives: superlativesArray });
